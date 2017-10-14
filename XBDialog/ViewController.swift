9a9bfd1db589055eb44b1cd0d843d1a7c6c7731f
xiaobin liu <327847390@qq.com>
2017-10-14 16:40:51 +0800

    -changed: 支持Swift4.0

--- XBDialog/ViewController.swift
+++ XBDialog/ViewController.swift
@@ -45,7 +45,6 @@ class ViewController: UIViewController {
 
 class DemoViewController: UIViewController {
     
-    
     private let button = UIButton(type: .custom)
     
     override func viewDidLoad() {
